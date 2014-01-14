b210283ec0291fa66ba446cbac5d3939c0e2e484
Jake Gregg <greggjs@miamioh.edu>
2014-01-14 16:58:36 -0500

    client written needs testing

--- rememerme/sessions/client.py
+++ rememerme/sessions/client.py
@@ -1,6 +1,22 @@
-from rest_framework.test import APIClient
+import requests
+from rememerme.session.models import Session
+from rest_framework.exceptions import APIException
+
+class YouDunFucked(APIException):
+	pass
 
 class SessionClient:
 	@staticmethod
 	def create(username, password):
-		APIClient().post('/rest/v1/')
+		payload = {'username':username, 'password':password}
+		r = requests.post('http://134.53.148.103:8000/rest/v1/',data=payload)
+		if r is not 200:
+			raise YouDunFucked()
+		return Session.fromMap(r.json())
+
+	@staticmethod
+	def update(session_id):
+		r = requests.put('http://134.53.148.103:8000/rest/v1/%s' % str(session_id) )
+		if r is not 200:
+			raise YouDunFucked()
+		return Session.fromMap(r.json())
