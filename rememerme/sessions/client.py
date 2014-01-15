75af7e8a2a9046a1a0e9646fcd9cfba3202a44b4
= <greggjs@miamioh.edu>
2014-01-15 23:52:28 +0000

    Client now works

--- rememerme/sessions/client.py
+++ rememerme/sessions/client.py
@@ -3,20 +3,21 @@ from rememerme.sessions.models import Session
 from rest_framework.exceptions import APIException
 
 class SessionClientError(APIException):
-	detail = ''
+	detail = 'error in connection to the server / it did not put right'
 
 class SessionClient:
 	@staticmethod
 	def create(username, password):
 		payload = {'username':username, 'password':password}
 		r = requests.post('http://134.53.148.103:8000/rest/v1/',data=payload)
-		if r is not 200:
-			raise YouDunFucked()
+		if r.status_code is not 200:
+			raise SessionClientError()
 		return Session.fromMap(r.json())
 
 	@staticmethod
 	def update(session_id):
 		r = requests.put('http://134.53.148.103:8000/rest/v1/%s' % str(session_id) )
-		if r is not 200:
-			raise YouDunFucked()
+		print r.status_code
+		if r.status_code is not 200:
+			raise SessionClientError()
 		return Session.fromMap(r.json())
